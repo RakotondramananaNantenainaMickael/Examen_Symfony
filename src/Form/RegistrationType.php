bc4e906a08a483abb048c65438effe75cd9ef955
RakotondramananaNantenainaMickael <nantenainamickael@yahoo.com>
2026-07-22 08:35:14 +0300

    Version finale

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -1,9 +1,9 @@
 <?php
+
 namespace App\Form;
 
 use App\Entity\Admin;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -11,11 +11,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class RegistrationType extends AbstractType {
-    public function buildForm(FormBuilderInterface $builder, array $options): void {
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
         $builder
             ->add('username', TextType::class, [
-                'label' => 'Nom utilisateur',
+                'label' => 'Nom d\'utilisateur',
                 'attr' => ['class' => 'form-control']
             ])
             ->add('password', PasswordType::class, [
@@ -26,7 +28,7 @@ class RegistrationType extends AbstractType {
                         'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length([
-                        'min' => 8,
+                        'min' => 6,
                         'minMessage' => 'Le mot de passe doit faire au moins {{ limit }} caractères',
                         'max' => 4096,
                     ]),
@@ -35,8 +37,9 @@ class RegistrationType extends AbstractType {
             ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void {
-        $resolver->setDefault([
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
             'data_class' => Admin::class,
         ]);
     }
--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Form;
 
 use App\Entity\Student;
@@ -7,8 +8,10 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class StudentType extends AbstractType {
-    public function buildForm(FormBuilderInterface $builder, array $options): void {
+class StudentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
         $builder
             ->add('firstName', TextType::class, [
                 'label' => 'Prénom',
@@ -33,8 +36,9 @@ class StudentType extends AbstractType {
             ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void {
-        $resolver->setDefault([
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
             'data_class' => Student::class,
         ]);
     }
